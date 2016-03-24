94a1d4624644ca4de6d8f7550d567b35f4f34302
Ian Lesikar and John Dow <ian@>
2016-03-24 14:03:48 -0700

    celebrity dating site v1 w/ branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,39 @@
 
 $(document).ready(function() {
-  console.log();
-  var voterAge = parseInt(prompt("How old are you?"));
-  if (voterAge >= 18) {
-    $("#adultPage").show();
-  } else {
-    $("#minorPage").show();
-  }
+  $("form#survey").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var orientation = $("select#orientation").val();
+
+    if (age > 18 && age < 35 && orientation === 'women') {
+      $('#tiffany').show();
+      $('#cassie').hide();
+      $('#jose').hide();
+      $('#alex').hide();
+    }
+    if (age >= 35 && orientation === 'women') {
+      $('#cassie').show();
+      $('#tiffany').hide();
+      $('#jose').hide();
+      $('#alex').hide();
+    }
+    if (age > 18 && age < 35 && orientation === 'men') {
+      $('#jose').show();
+      $('#tiffany').hide();
+      $('#cassie').hide();
+      $('#alex').hide();
+    }
+    if (age >= 35 && orientation === 'men') {
+      $('#alex').show();
+      $('#tiffany').hide();
+      $('#cassie').hide();
+      $('#jose').hide();
+    }
+    if (age < 18) {
+      alert('No matches available for your age group.');
+    }
+    if (orientation === 'other') {
+      alert('No matches available for your orientation.')
+    }
+    event.preventDefault();
+  });
 });
